3117bf6b5323783bde026ee771a057caaaa804f2
Paul Kiddle <neonpaul@gmail.com>
2015-10-03 13:24:33 +0100

    Moved most of the JS code into one place, added basic grunt & package files

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'app/**/*.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('default', ['jshint']);
+
+};
